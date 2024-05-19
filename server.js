66e70cf9fdc8be16d6238f6ca56b717f785faf81
Remy <remy.frigg@gmail.com>
2024-05-19 21:54:43 +0200

    update_1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-)))');
+  res.send('Hello DevOps FS2024 Woche 13 Remy');
 });
 
 app.listen(PORT, HOST);
