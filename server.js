ff18b6c1510f4b70f3026341a592158a01f7fd8c
Remy <remy.frigg@gmail.com>
2024-05-20 12:25:34 +0200

    Update 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 Woche 13 Remy mit Docker (workflow enabled)');
+  res.send('Hello DevOps FS2024 Woche 13 Remy mit Docker (workflow enabled) test 2');
 });
 
 app.listen(PORT, HOST);
